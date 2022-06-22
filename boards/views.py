ab4a83b8ee496686621b22d4dd87364674acd7da
Nik ILyushkin <nikolay.ilyushkin@me.com>
2022-06-22 21:34:54 +1200

    views.py fix

--- boards/views.py
+++ boards/views.py
@@ -28,7 +28,8 @@ def board(request, board_slug):
         return cached_page
 
     blocks = BoardBlock.objects.filter(board=board)
-    feeds = BoardFeed.objects.select_related("articles").filter(board=board)
+#    feeds = BoardFeed.objects.select_related("articles").filter(board=board)
+    feeds = BoardFeed.objects.filter(board=board)
     result = render(request, "board.html", {
         "board": board,
         "blocks": blocks,
